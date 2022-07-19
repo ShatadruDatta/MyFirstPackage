9023601c1cc6d3080de87ec4f23c42ab947d6764
Shatadru Datta <shatadru.datta.contractor@pepsico.com>
2022-07-19 19:15:06 +0530

    Added a basic example function.

--- Sources/MyFirstPackage/MyFirstPackage.swift
+++ Sources/MyFirstPackage/MyFirstPackage.swift
@@ -2,5 +2,10 @@ public struct MyFirstPackage {
     public private(set) var text = "Hello, World!"
 
     public init() {
+        
+    }
+    
+    public func helloToYou() -> String {
+        return "Hi, Shatadru"
     }
 }
